bd1cf57a616b5e66ccda2af2913de54f41c0d7b1
tomchen519 <tom.chen519@gmail.com>
2017-10-15 16:32:51 -0700

    python autocomplete

--- .pythonrc.py
+++ .pythonrc.py
@@ -5,3 +5,6 @@ except ImportError:
 else:
   import rlcompleter
   readline.parse_and_bind("tab: complete")
+
+# add following line to .bashrc or .bash_profile 
+# export PYTHONSTARTUP=$HOME/.pythonrc.py
